0c9e7a80fcd8d274cb54ca6eb7e846fa10a7ff8a
Gabriele Russo <64378785+grusso02@users.noreply.github.com>
2021-02-14 16:13:34 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 10:12:36 by grusso            #+#    #+#             */
+/*   Updated: 2021/02/02 16:26:11 by grusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# define OPEN_MAX 256
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strnew(size_t size);
+char	*ft_strcpy(char *dest, char *src);
+
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 10:57:08 by grusso            #+#    #+#             */
+/*   Updated: 2021/02/03 13:10:36 by grusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	l;
+	size_t	i;
+	char	*cpy;
+
+	i = 0;
+	l = ft_strlen(s1);
+	if (!(cpy = (char *)malloc(l + 1 * sizeof(char))))
+		return (0);
+	while (s1[i])
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int			idx;
+	int			idx_join;
+	char		*join;
+
+	if (!s1 || !s2 || !(join = malloc(sizeof(char) *
+		(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1))))
+		return (NULL);
+	idx = 0;
+	idx_join = 0;
+	while (s1[idx])
+		join[idx_join++] = s1[idx++];
+	idx = 0;
+	while (s2[idx])
+		join[idx_join++] = s2[idx++];
+	join[idx_join] = '\0';
+	return (join);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] && (unsigned char)c != s[i])
+		i++;
+	if (s[i] == (unsigned char)c)
+		return ((char *)s + i);
+	return (0);
+}
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+
+	if (!(str = (char *)malloc(sizeof(char) * size + 1)))
+		return (NULL);
+	str[size] = '\0';
+	while (size--)
+		str[size] = '\0';
+	return (str);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
