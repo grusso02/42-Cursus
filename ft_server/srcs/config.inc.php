f2d47f90d2ea7465af6228d2b817c806620bf63f
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-02-28 19:45:01 +0100

    finish_ft_server

--- ft_server/srcs/config.inc.php
+++ ft_server/srcs/config.inc.php
@@ -9,13 +9,13 @@
  *
  * @package PhpMyAdmin
  */
-declare(strict_types=1);
+// declare(strict_types=1);
 
 /**
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'abcdefghijklmnopqrstuvwxyz0123456789' /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'abcdefghijklmnopqrstuvwxyz0123456789'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
