779bbff5ba27dc6dc6ba4875c24bba7b9ac502c0
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-04-18 17:08:44 +0200

    start_nginx_ft_services

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -3,24 +3,21 @@
 #                                                         :::      ::::::::    #
 #    setup.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: grusso <marvin@42.fr>                      +#+  +:+       +#+         #
+#    By: grusso <grusso@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/16 18:39:45 by grusso            #+#    #+#              #
-#    Updated: 2021/04/17 16:41:38 by grusso           ###   ########.fr        #
+#    Updated: 2021/04/18 17:03:33 by grusso           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-function metallinstall()
-{
-	printf "\e[39m START BUILDING \e[5;92mmetallb\e[0m\r"
-	start=`date +%s`
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml >> last.log
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml >> last.log
-	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" >> last.log
-	kubectl apply -f srcs/metallb/. >> last.log
-	end=`date +%s`
-	runtime=$((end-start))
-	printf "\e[K\e[92m metallb DONE (in ${runtime}s)!\e[0m\n"
-}
+# Install MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb/.
+
+docker build srcs/nginx/. -t nginx-image
+kubectl apply -f srcs/nginx/yaml/.
