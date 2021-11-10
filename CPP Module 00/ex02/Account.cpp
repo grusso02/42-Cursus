e177526b4c7070dfd96beed05d3d6e55f567a6fe
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-11-10 20:19:31 +0100

    CPP_ex02

--- CPP Module 00/ex02/Account.cpp
+++ CPP Module 00/ex02/Account.cpp
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriele <gabriele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:30:28 by gabriele          #+#    #+#             */
-/*   Updated: 2021/11/02 13:57:37 by gabriele         ###   ########.fr       */
+/*   Updated: 2021/11/10 17:34:02 by grusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
 int	Account::getNbAccounts(void)
 {
@@ -32,7 +33,12 @@ int	Account::getNbWithdrawals(void)
 	return (_totalNbWithdrawals);
 }
 
-void Account::displayAccountsInfos(void)
+/* void Account::displayAccountsInfos(void)
+{
+
+} */
+
+Account::Account(int initial_deposit)
 {
 	
 }
