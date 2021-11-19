7c82896783b254c106fefb52978ed474c46ec012
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-11-19 18:40:23 +0100

    end_CPP_Module01

--- CPP Module 01/ex06/Karen.cpp
+++ CPP Module 01/ex06/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:13:44 by grusso            #+#    #+#             */
-/*   Updated: 2021/11/19 18:06:10 by grusso           ###   ########.fr       */
+/*   Updated: 2021/11/19 18:36:01 by grusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,55 @@ Karen::~Karen() {}
 
 void	Karen::debug(void)
 {
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI just love it!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::info(void)
 {
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon cost more money.\nYou don’t put enough! If you did I would not have to ask for it!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::warning(void)
 {
+	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::error(void)
 {
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Karen::complain(std::string level)
 {
 	std::string	ref[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	for (int i = 0; i < 4; i++)
-		if (level == ref[i])
-			return (this->*(_levels[i]))();
+	int i = 0;
+	for (; i < 4 && ref[i] != level;)
+		i++;
+
+	enum levels {DEBUG, INFO, WARNING, ERROR};
+	switch (i)
+	{
+		case DEBUG:
+			(this->*(_levels[DEBUG]))();
+		case INFO:
+			(this->*(_levels[INFO]))();
+		case WARNING:
+			(this->*(_levels[WARNING]))();
+		case ERROR:
+			(this->*(_levels[ERROR]))();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    		std::cout << std::endl;
+			break;
+	}
 }
---
+++ CPP Module 01/ex06/Karen.hpp
@@ -0,0 +1,22 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+#include <iostream>
+#include <string>
+
+class Karen
+{
+	public:
+		Karen();
+		~Karen();
+		void	complain(std::string level);
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		void	(Karen::*_levels[4])();
+};
+
+
+#endif
