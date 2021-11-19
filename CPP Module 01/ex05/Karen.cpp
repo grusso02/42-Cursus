3ffce8cd6cb944a5fd5cc4066325ed1bda684dd5
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-11-19 18:02:46 +0100

    end_ex05

---
+++ CPP Module 01/ex05/Karen.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 17:13:44 by grusso            #+#    #+#             */
+/*   Updated: 2021/11/19 17:48:59 by grusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+Karen::Karen()
+{
+	_levels[0] = &Karen::debug;
+	_levels[1] = &Karen::info;
+	_levels[2] = &Karen::warning;
+	_levels[3] = &Karen::error;
+}
+
+Karen::~Karen() {}
+
+void	Karen::debug(void)
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void	Karen::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void	Karen::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void	Karen::error(void)
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain(std::string level)
+{
+	std::string	ref[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+		if (level == ref[i])
+			return (this->*(_levels[i]))();
+}
