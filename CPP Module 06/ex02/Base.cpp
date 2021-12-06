2336bb25bcfd823bd0652550248e1699ce59a149
Gabriele Russo <grusso@e3r9p2.42roma.it>
2021-12-06 21:18:55 +0100

    end_ex02

---
+++ CPP Module 06/ex02/Base.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 20:40:00 by grusso            #+#    #+#             */
+/*   Updated: 2021/12/06 21:18:06 by grusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+
+Base*	generate(void)
+{
+	int i = rand() % 3;
+
+	if (!i)
+		return (new A);
+	else if (i == 1)
+		return (new B);
+	else
+		return (new C);
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Cast return NULL" << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		(void)e;
+	}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		(void)e;
+	}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		(void)e;
+	}
+}
--- CPP Module 06/ex02/main.cpp
+++ CPP Module 06/ex02/main.cpp
@@ -5,40 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/06 19:57:21 by grusso            #+#    #+#             */
-/*   Updated: 2021/12/06 21:12:09 by grusso           ###   ########.fr       */
+/*   Created: 2021/12/06 20:46:16 by grusso            #+#    #+#             */
+/*   Updated: 2021/12/06 21:16:06 by grusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-#include <iostream>
-#include <ctime>
-
-struct Data
-{
-	int nb;
-};
-
-uintptr_t	serialize(Data* ptr)
-{
-	return (reinterpret_cast<uintptr_t>(ptr));
-}
-
-Data*		deserialize(uintptr_t raw)
-{
-	return (reinterpret_cast<Data*>(raw));
-}
+#include "Base.hpp"
 
 int main()
 {
-	Data*		data = new Data;
-	Data*		data2;
-	uintptr_t	raw = 0;
- 
-	data->nb = 15;
- 	raw = serialize(data);
-	data2 = deserialize(raw);
-	std::cout << "Numero: " << data2->nb << std::endl;
-	delete data;
+	srand(time(0));
+	Base *ptr = nullptr;
+
+	ptr = generate();
+	identify(ptr);
+	identify(*ptr);
+	delete ptr;
+
 	return 0;
 }
