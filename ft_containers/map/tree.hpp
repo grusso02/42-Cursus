a49a58b9ddbbf03a1db505a32b2a85c2e17596d5
Gabriele Russo <grusso@e3r9p2.42roma.it>
2022-04-11 16:32:43 +0200

    42 two

--- ft_containers/map/tree.hpp
+++ ft_containers/map/tree.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tree.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriele <gabriele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: grusso <grusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 15:15:40 by grusso            #+#    #+#             */
-/*   Updated: 2022/04/05 16:18:20 by gabriele         ###   ########.fr       */
+/*   Updated: 2022/04/11 16:16:14 by grusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,18 @@ struct Node
 	~Node() {}
 };
 
-template < class Value, class Compare = std::less<Value>, class Alloc = std::allocator<Value> >
+template < class Value, class Compare = std::less<Value>, class Alloc = std::allocator<Node<Value> > >
 class _tree
 {
 	public:
 		typedef _Tp					value_type;
 		typedef _Compare			value_compare;
 		typedef _Allocator			allocator_type;
+
+		typedef typename allocator_type::pointer				pointer;
+		typedef typename allocator_type::const_pointer			const_pointer;
+		typedef typename allocator_type::size_type       		size_type;
+		typedef typename allocator_type::difference_type 		difference_type;
+		typedef typename allocator_type::reference				reference;
+		typedef typename allocator_type::const_reference		const_reference;
 };
